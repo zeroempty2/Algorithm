80f7d3521f53e20c673188adab6ffc0509af3d73
0empty <117061586+zeroempty2@users.noreply.github.com>
2023-04-25 22:26:13 +0900

    [unrated] Title: 덧칠하기, Time: 0.84 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -1,13 +1,15 @@
 class Solution {
-     public int solution(int n, int m, int[] section) {
-        int roller = section[0];
-        int cnt = 1;
-        for(int i = 1; i < section.length; i++) {
-            if(roller + m - 1 < section[i]) {
-                cnt++;
-                roller = section[i];
-            }
-        }
-        return cnt;
+      public int solution(int n, int m, int[] section) {
+    int answer = 0;
+    int roller = 1;
+
+    for (int j : section) {
+      if (roller <= j) {
+        roller = j + m;
+        answer++;
+      }
     }
+
+    return answer;
   }
+}
