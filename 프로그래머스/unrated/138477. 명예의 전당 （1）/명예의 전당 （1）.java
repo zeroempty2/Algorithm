3ffd489b4dab601d076fae398f3ee0e639faad76
0empty <117061586+zeroempty2@users.noreply.github.com>
2023-05-13 23:04:20 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.33 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for(int i = 0; i < score.length; i++) {
+            priorityQueue.add(score[i]);
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
+            }
+            answer[i] = priorityQueue.peek();
+        }
+        return answer;
+    }
+}
