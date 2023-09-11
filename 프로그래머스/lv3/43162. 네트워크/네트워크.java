6a612af65c1b20ef30a160078ecaa3a9e5e82934
0empty <117061586+zeroempty2@users.noreply.github.com>
2023-09-11 19:55:16 +0900

    [level 3] Title: 네트워크, Time: 0.13 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,44 +1,21 @@
-import java.util.Stack;
- 
 class Solution {
-    boolean[] visited;
-
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        visited = new boolean[n];
-        
-        Stack<Integer> stack = new Stack<>();
- 
-        for (int i = 0; i < computers.length; i++) {
-            if (visited[i]) {
-                continue;
-            }
- 
-            stack.push(i);
-            answer++;
- 
-            while(!stack.isEmpty()) {
-                int cur = stack.pop();
- 
-                if (visited[cur]) {
-                    continue;
-                }
- 
-                visited[cur] = true;
- 
-                int[] computer = computers[cur];
- 
-                for (int j = 0; j < computer.length; j++) {
-                    if (visited[j] || computer[j] == 0){
-                        continue;
-                    }
- 
-                    stack.push(j);
-                }
+        boolean[] chk = new boolean[n];
+        for(int i = 0; i < n; i++) {
+            if(!chk[i]) {
+                dfs(computers, chk, i);
+                answer++;
             }
         }
- 
- 
         return answer;
+    }
+    void dfs(int[][] computers, boolean[] chk, int start) {
+        chk[start] = true;
+        for(int i = 0; i < computers.length; i++) {
+            if(computers[start][i] == 1 && !chk[i]) {
+                dfs(computers, chk, i);
+            }
+        }
     }
 }
