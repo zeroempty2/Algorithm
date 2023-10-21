7683d88ae4368ee016b2db5998fbe6c76ef46e9d
0empty <117061586+zeroempty2@users.noreply.github.com>
2023-10-21 18:36:01 +0900

    [level 3] Title: 단속카메라, Time: 6.08 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 1;
+        Arrays.sort(routes,(o1,o2)->(o1[1]-o2[1]));
+        int max = routes[0][1];
+        for(int i = 1; i < routes.length; i++){
+            if(routes[i][0] <= max && routes[i][1] >= max) continue;
+            else{
+                max = routes[i][1];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
