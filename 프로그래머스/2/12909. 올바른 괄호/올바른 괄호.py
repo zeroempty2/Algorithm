4b7b70818bfcf833c71f1d6f49af84021d5cde37
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-10-13 20:07:28 +0900

    [level 2] Title: 올바른 괄호, Time: 17.99 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,16 @@
+def solution(s):
+    arr = list(s)
+    stack = []
+    
+    for i in range(len(arr)):
+        if len(stack) != 0:
+            if arr[i] == ')' and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(arr[i])
+        else:
+            stack.append(arr[i])
+    
+    answer = True if len(stack) == 0 else False
+    
+    return answer
