f33a22213a0227eb2d53e8c7befde051a4b10644
0empty <117061586+zeroempty2@users.noreply.github.com>
2024-11-27 01:17:07 +0900

    [Silver V] Title: 집합, Time: 1048 ms, Memory: 312476 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -2,29 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int M = Integer.parseInt(bf.readLine());
         StringBuilder sb = new StringBuilder();
         int S = 0;
-        int M = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        while (M-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            String str = st.nextToken();
-            if(str.equals("all")) S = (1 << 21) - 1;
-            else if(str.equals("empty")) S = 0;
-            else {
-                int num = Integer.parseInt(st.nextToken());
-                if(str.equals("add"))
-                    S |= (1 << num);
-                else if(str.equals("remove"))
-                    S &= ~(1 << num);
-                else if(str.equals("check"))
-                    sb.append((S & (1 << num)) != 0 ? 1 : 0).append("\n");
-                else if(str.equals("toggle"))
-                    S ^= (1 << num);
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(bf.readLine());
+            String action = st.nextToken();
+            if (action.equals("all")) S = (1 << 21) - 1;
+            else if (action.equals("empty")) S = 0;
+            else{
+             int num = Integer.parseInt(st.nextToken());
+                if (action.equals("add")) S |= (1 << num);  
+                else if (action.equals("remove")) S &= ~(1 << num); 
+                else if (action.equals("check")) sb.append((S & (1 << num)) != 0 ? 1 : 0).append("\n");
+                else if (action.equals("toggle")) S ^= (1 << num);
             }
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
