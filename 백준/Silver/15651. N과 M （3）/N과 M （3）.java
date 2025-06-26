69b1291104ac1ee7ed1231ee433ca0f9ff630a50
0empty <117061586+zeroempty2@users.noreply.github.com>
2025-06-26 15:26:46 +0900

    [Silver III] Title: N과 M (3), Time: 384 ms, Memory: 67856 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        result = new int[M];
+
+        backtrack(0);
+        System.out.print(sb);
+    }
+
+    static void backtrack(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            result[depth] = i;
+            backtrack(depth + 1);
+        }
+    }
+}
