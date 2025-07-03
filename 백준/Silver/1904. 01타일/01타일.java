70494b328f53d6c378190055761ed5c9743de772
0empty <117061586+zeroempty2@users.noreply.github.com>
2025-07-03 18:35:53 +0900

    [Silver III] Title: 01타일, Time: 112 ms, Memory: 18204 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final int MOD = 15746;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        int[] dp = new int[N + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
