be7ba7721d604fc88c0a9143f486215901a5a8c2
0empty <117061586+zeroempty2@users.noreply.github.com>
2025-08-12 17:43:44 +0900

    [Silver V] Title: 배열 합치기, Time: 1328 ms, Memory: 300960 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+        int[] B = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int i = 0, j = 0;
+
+        // 두 배열을 병합
+        while (i < N && j < M) {
+            if (A[i] <= B[j]) {
+                sb.append(A[i++]).append(" ");
+            } else {
+                sb.append(B[j++]).append(" ");
+            }
+        }
+
+        // A 배열 남은 부분
+        while (i < N) sb.append(A[i++]).append(" ");
+        // B 배열 남은 부분
+        while (j < M) sb.append(B[j++]).append(" ");
+
+        System.out.println(sb);
+    }
+}
