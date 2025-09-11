61a24d27489d1ab0c1fb94f9a38fc6cdb2aae1ce
0empty <117061586+zeroempty2@users.noreply.github.com>
2025-09-11 16:32:13 +0900

    [Silver V] Title: 행렬 곱셈, Time: 160 ms, Memory: 16476 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] A = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int M2 = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][] B = new int[M2][K];
+        for (int i = 0; i < M2; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < K; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 결과 행렬 C = A * B
+        int[][] C = new int[N][K];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                int sum = 0;
+                for (int t = 0; t < M; t++) {
+                    sum += A[i][t] * B[t][j];
+                }
+                C[i][j] = sum;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                sb.append(C[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
