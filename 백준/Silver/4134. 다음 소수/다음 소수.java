e2b89bb10fba62818187883599b060949c69ff48
0empty <117061586+zeroempty2@users.noreply.github.com>
2025-09-28 14:52:16 +0900

    [Silver IV] Title: 다음 소수, Time: 484 ms, Memory: 21240 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    static boolean isPrime(long n) {
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        long sqrt = (long)Math.sqrt(n);
+        for (long i = 3; i <= sqrt; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    static long nextPrime(long n) {
+        if (n <= 2) return 2;
+        if (n % 2 == 0) n++;
+        while (!isPrime(n)) {
+            n += 2;
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            long n = sc.nextLong();
+            System.out.println(nextPrime(n));
+        }
+    }
+}
