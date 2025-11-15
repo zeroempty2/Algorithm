fb5306fac470d212472ee8ef861bfb031639bb9b
0empty <117061586+zeroempty2@users.noreply.github.com>
2025-11-15 14:47:41 +0900

    [Silver V] Title: 배열 합치기, Time: 1284 ms, Memory: 300880 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,17 +1,21 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
+        //배열 A,B의 길이 N,M
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         int[] A = new int[N];
         int[] B = new int[M];
 
+        // 배열 채우기
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
@@ -23,22 +27,34 @@ public class Main {
         }
 
         StringBuilder sb = new StringBuilder();
-        int i = 0, j = 0;
-
-        // 두 배열을 병합
-        while (i < N && j < M) {
-            if (A[i] <= B[j]) {
-                sb.append(A[i++]).append(" ");
-            } else {
-                sb.append(B[j++]).append(" ");
+        int a = 0 , b = 0, point = 0;
+        
+        //값 비교하기
+        for(int i = 0; i < N+M; i++){
+            if(a < N && b < M){
+                if (A[a] <= B[b]) {
+                    sb.append(A[a++]).append(" ");
+                } 
+                else {
+                    sb.append(B[b++]).append(" ");
+                }
+            }
+            else{
+                point = i;
+                break;
             }
         }
 
-        // A 배열 남은 부분
-        while (i < N) sb.append(A[i++]).append(" ");
-        // B 배열 남은 부분
-        while (j < M) sb.append(B[j++]).append(" ");
+        //남은 부분
+        if(a < N){
+            for(int i = point; i < N+M; i++) sb.append(A[a++]).append(" ");
+            
+        }
+        else if(b < M){
+            for(int i = point; i < N+M; i++) sb.append(B[b++]).append(" ");
+        }
 
         System.out.println(sb);
+            
     }
 }
