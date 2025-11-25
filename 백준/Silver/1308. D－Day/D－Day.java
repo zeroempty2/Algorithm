7dddbb92c2cb0b3f869fe08b5423a23dc384b5fc
0empty <117061586+zeroempty2@users.noreply.github.com>
2025-11-25 14:36:37 +0900

    [Silver V] Title: D-Day, Time: 180 ms, Memory: 18344 KB -BaekjoonHub

--- 백준/Silver/1308. D－Day/D－Day.java
+++ 백준/Silver/1308. D－Day/D－Day.java
@@ -1,51 +1,31 @@
 import java.util.*;
 
 public class Main {
-    static int[] daysInMonth = {0,
-        31, 28, 31, 30, 31, 30,
-        31, 31, 30, 31, 30, 31
-    };
+
+    static int[] md = {0,31,28,31,30,31,30,31,31,30,31,30,31};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int y1 = sc.nextInt(), m1 = sc.nextInt(), d1 = sc.nextInt();
-        int y2 = sc.nextInt(), m2 = sc.nextInt(), d2 = sc.nextInt();
+        int y1=sc.nextInt(), m1=sc.nextInt(), d1=sc.nextInt();
+        int y2=sc.nextInt(), m2=sc.nextInt(), d2=sc.nextInt();
 
         if (y2 > y1 + 1000 ||
-           (y2 == y1 + 1000 && m2 > m1) ||
-           (y2 == y1 + 1000 && m2 == m1 && d2 >= d1)) {
+            (y2 == y1 + 1000 && (m2 > m1 || (m2 == m1 && d2 >= d1)))) {
             System.out.println("gg");
             return;
         }
 
-        int startDays = convertToDays(y1, m1, d1);
-        int endDays = convertToDays(y2, m2, d2);
-
-        System.out.println("D-" + (endDays - startDays));
+        System.out.println("D-" + (toDays(y2,m2,d2) - toDays(y1,m1,d1)));
     }
 
-
-    static int convertToDays(int year, int month, int day) {
-        int days = 0;
-
-        for (int i = 1; i < year; i++) {
-            days += isLeap(i) ? 366 : 365;
-        }
-
-        for (int i = 1; i < month; i++) {
-            if (i == 2 && isLeap(year)) days += 29;
-            else days += daysInMonth[i];
-        }
-
-        days += day;
-
-        return days;
+    static int toDays(int y, int m, int d) {
+        int days = 365*(y-1) + (y-1)/4 - (y-1)/100 + (y-1)/400;
+        for(int i=1;i<m;i++) days += (i==2 && leap(y)?29:md[i]);
+        return days + d;
     }
-    
-    static boolean isLeap(int y) {
-        if (y % 400 == 0) return true;
-        if (y % 100 == 0) return false;
-        return y % 4 == 0;
+
+    static boolean leap(int y) {
+        return y%400==0 || (y%4==0 && y%100!=0);
     }
 }
